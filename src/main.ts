fd4031fbaaabc64615fa084753d04808076aac41
xyfy <xyfy@live.cn>
2020-08-06 17:46:23 +0800

    add ant-vue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+//import Button from 'ant-design-vue/lib/button';
+//Vue.component(Button.name, Button);
 
 Vue.config.productionTip = false;
 
---
+++ src/types/index.d.ts
@@ -0,0 +1,30 @@
+declare module 'ant-design-vue/es/notification' {
+  const Ant: any
+  export default Ant;
+}
+
+declare module 'ant-design-vue/es/message' {
+  const Ant: any
+  export default Ant;
+}
+
+declare module 'ant-design-vue/lib/locale-provider/zh_CN' {
+  const Ant: any
+  export default Ant;
+}
+
+declare module 'ant-design-vue' {
+  const Ant: any
+  export default Ant;
+}
+
+declare module '@/assets/icon/renzheng.svg' {
+  const svg: any
+  export default svg;
+}
+
+// declare module 'vue-photo-preview-fork' {
+//   const svg: any
+//   export default svg;
+// }
+
