ed6bf229f319760fa442539e50e988e7fb61d845
xyfy <xyfy@live.cn>
2020-08-11 13:40:41 +0800

    u

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-let Configs = {
+const Configs = {
   apis: {
     default: {
       url: 'http://localhost:65115'
@@ -15,21 +15,6 @@ let Configs = {
   }
 }
 if (process.env.NODE_ENV === 'production') {
-  Configs = {
-    apis: {
-      default: {
-        url: 'https://api.dev.boligc.com'
-      }
-    },
-    authConfig: {
-      issuer: 'http://localhost:64999',
-      clientId: 'app-client',
-      dummyClientSecret: 'winkappsercret',
-      // scope: "OrganizationDemo",
-      showDebugInformation: true,
-      oidc: false,
-      requireHttps: true
-    }
-  }
+
 }
 export default Configs
